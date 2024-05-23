ddc1c5a9aea315a880e61210fcb9b94c10e775db
praveen <praveensatalgaon1@gmail.com>
2024-05-23 08:15:48 +0530

    add jexl demo app

---
+++ JEXL_demo/src/java/FirstDemo.java
@@ -0,0 +1,14 @@
+package java;
+
+
+public class FirstDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.print("hi");
+       
+        
+	}
+
+}
