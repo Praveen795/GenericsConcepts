ffd0c3913bdfc911437a57b09df5f84d5d29fa5c
praveen <praveensatalgaon1@gmail.com>
2024-07-08 10:24:12 +0530

    add generic project

--- BookStore/src/main/java/com/BookStore/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.BookStore.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.BookStore.dao.Book;
-
-public interface BookRepository extends JpaRepository<Book, Integer>{
-
-}
